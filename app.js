49202b06284e21e68d690a481d78e4abbca7141d
AndyLam <andy.lam@upbreeze.com>
2019-01-18 19:42:36 +0800

    added passport and mongoose integration

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+//  const favicon = require('serve-favicon');
 const cookieParser = require('cookie-parser');
 const lessMiddleware = require('less-middleware');
 const logger = require('morgan');
 
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+const flash = require('connect-flash');
+const mongoose = require('mongoose');
+
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
+//  app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,24 +27,48 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+	secret: 'Please change the secret of the application',
+	resave: true,
+	saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(flash());
+app.use(passport.session());
+
+// requires the model with Passport-Local Mongoose plugged in
+const User = require('./models/user');
+
+// use static authenticate method of model in LocalStrategy
+passport.use(new LocalStrategy(User.authenticate()));
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Connect mongoose
+mongoose.connect('mongodb://localhost/users', (err) => {
+	if (err) {
+		console.log('Could not connect to mongodb on localhost. Ensure that you have mongodb running on localhost and mongodb accepts connections on standard ports!');
+	}
+});
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,42 @@
 const express = require('express');
+const passport = require('passport');
+const Account = require('../models/user');
 
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+	res.render('index', { user: req.user });
+});
+
+router.get('/register', (req, res) => {
+	res.render('register', {});
+});
+
+router.post('/register', (req, res, next) => {
+	console.log('registering user');
+	Account.register(new Account({ username: req.body.username }), req.body.password, (err) => {
+		if (err) {
+			console.log('error while user register!', err);
+			return next(err);
+		}
+
+		console.log('user registered!');
+
+		res.redirect('/');
+	});
+});
+
+router.get('/login', (req, res) => {
+	res.render('login', { user: req.user });
+});
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+	res.redirect('/');
+});
+
+router.get('/logout', (req, res) => {
+	req.logout();
+	res.redirect('/');
 });
 
 module.exports = router;
